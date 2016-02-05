fd620a84ce610940d0ac85ae7b3932645b92b098
Marek Smigielski <m.smigielski@oberthur.com>
2016-02-05 21:25:07 +0100

    fixes

---
+++ Watch/BleManager.swift
@@ -0,0 +1,13 @@
+//
+//  BleManager.swift
+//  Watch
+//
+//  Created by Marek Smigielski on 20/01/16.
+//  Copyright © 2016 ossw. All rights reserved.
+//
+
+import Foundation
+
+class BleManager{
+    
+}
